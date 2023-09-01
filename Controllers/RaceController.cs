4c3735e3ed04240c2a9fa85a9575b863b3d3b287
bocharoviv <bocharoviv@bukep.ru>
2023-09-01 16:45:38 +0300

    Добавил страницы для карточек

---
+++ Controllers/RaceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Race> races = _context.Races.ToList();
+            return View(races);
+        }
+        public IActionResult Detail(int id)
+        {
+            Race race = _context.Races.Include(x => x.Address).FirstOrDefault(c => c.Id == id);
+            return View(race);
+        }
+    }
+}
